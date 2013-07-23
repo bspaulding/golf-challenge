c67506c187c52fe934fe9f3eac3216e9509cbd8f
Bradley Spaulding <brad.spaulding@gmail.com>
2013-07-23 18:38:56 -0400

    Using FixtureAdaptor until Parse is working right.

--- js/app.js
+++ js/app.js
@@ -5,11 +5,7 @@ DS.RecordArray.reopen({
 App = Ember.Application.create();
 
 App.Store = DS.Store.extend({
-  adapter: ParseAdapter.create({
-    applicationId: 'RtuTUP4UioN4Jk51mMO3epOZbEI0pmHTdprFAL9n',
-    javascriptId: 'j24CboJmqSeY1p4YELdyw7SCypWHA20VeH3eqGcL',
-    restApiId: 'NMbhVqcUGUqb2tT1AhRE1HyzlMCSEhdLoJyNbMBv'
-  })
+  adapter: DS.FixtureAdapter
 });
 
 App.Router.map(function() {
