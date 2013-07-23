8f60ce9be80902cb7a51e5fb5bd77627779c1a60
Bradley J. Spaulding <bspaulding@dataxu.com>
2013-07-23 12:38:57 -0400

    Moved IndexRoute into separate file.

--- js/app.js
+++ js/app.js
@@ -19,9 +19,3 @@ App.Router.map(function() {
   });
 });
 
-App.IndexRoute = Ember.Route.extend({
-  redirect: function() {
-    this.transitionTo('brackets.edit', { bracket: App.Bracket.find('C2OkmPoiOe') });
-  }
-});
-
