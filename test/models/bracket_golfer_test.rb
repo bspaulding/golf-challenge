1efc8bcd076d9bbf94034d59c162d131eae2ca3a
Bradley J. Spaulding <bspaulding@dataxu.com>
2013-07-27 18:41:29 -0400

    Removed test dir.

--- test/models/bracket_golfer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BracketGolferTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/golfer_bracket_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GolferBracketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
