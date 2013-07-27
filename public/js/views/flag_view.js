d83c3836a0f74c908a95a45d44289cf627ec3a0a
Bradley J. Spaulding <bspaulding@dataxu.com>
2013-07-27 19:34:44 -0400

    Refactored FlagView styles into css.

--- public/js/views/flag_view.js
+++ public/js/views/flag_view.js
@@ -1,8 +1,8 @@
 App.FlagView = Ember.View.extend({
   country: null,
   tagName: 'img',
-  attributeBindings: ['src', 'style'],
-  style: "opacity:0.3;height:36px;",
+  attributeBindings: ['src'],
+  classNames: ['flag'],
   src: function() {
     if ( this.get('country.name') ) {
       return "/img/country-flag-icons/" + this.get('country.name').replace(/ /g, "-") + "-Flag.png";
